99619235fcf56b0394440684392b5469e755b629
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-23 02:51:34 +0300

    import clean

--- src/main/java/io/ssstoyanov/dvd2/controllers/UserController.java
+++ src/main/java/io/ssstoyanov/dvd2/controllers/UserController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
-
 @RequestMapping("/api/v1/users")
 @RestController
 public class UserController {

cc8e5c590aa9d06e1f36e0950c6794d95fa49db5
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-23 02:50:37 +0300

    enable swagger config and registration

--- src/main/java/io/ssstoyanov/dvd2/configurations/SecurityConfiguration.java
+++ src/main/java/io/ssstoyanov/dvd2/configurations/SecurityConfiguration.java
@@ -4,14 +4,12 @@ import io.ssstoyanov.dvd2.services.MongoUserDetailsService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableConfigurationProperties
@@ -27,9 +25,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/v1/users/**",
-                        "/swagger.html",
-                        "/swagger-ui/**",
+                .antMatchers(HttpMethod.POST, "/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/swagger-ui/index.html",
                         "/api-docs",
                         "/api-docs.yaml").permitAll()
                 .anyRequest().authenticated()
@@ -38,7 +36,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin().disable();
 
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,12 +47,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         builder.userDetailsService(userDetailsService);
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }
 }
